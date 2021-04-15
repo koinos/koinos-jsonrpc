73401fd29366ff1d0a751bceba117f099991d0d2
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-04-15 13:02:55 -0700

    Fix after_success.sh

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ -z $BUILD_DOCKER ]]; then
-   #coveralls-lcov --repo-token "$COVERALLS_REPO_TOKEN" --service-name travis-pro ./build/merged.info
-else
+#coveralls-lcov --repo-token "$COVERALLS_REPO_TOKEN" --service-name travis-pro ./build/merged.info
+
+if ! [[ -z $BUILD_DOCKER ]]; then
    TAG="$TRAVIS_BRANCH"
    if [ "$TAG" = "master" ]; then
       TAG="latest"
